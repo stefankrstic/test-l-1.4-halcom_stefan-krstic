cd0239a8ade09618166d6752689f12e1b2588bd4
Stefan <stefan.k.krstic@gmail.com>
2021-12-21 21:16:21 +0100

    Collect click info and stop logic

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { BaseButton } from "../components/BaseButton";
 import './TestPage.scss'
 import { Row, Col, Container } from 'react-bootstrap';
@@ -6,44 +6,89 @@ import { Header } from "../components/Header";
 import { Modal } from '../components/Modal';
 import { UserContext } from "../context/UserContext";
 
+const buttons = [1, 2, 3, 4, 5];
+
 export const TestPage = () => {
     const user = useContext(UserContext);
+    const [clicks, setClicks] = useState<Array<number>>([]);
+    const [isRecording, setIsRecording] = useState(true);
+
+    const handleClick = (button: number) => () => {
+        if (isRecording) {
+            setClicks([...clicks, button]);
+        }
+    };
+
+    const handleStopClick = () => {
+        setIsRecording(false);
+    };
+
+    const getButtonColor = (button: number) => {
+        if (isRecording) {
+            return lastClick === button ? 'yellow' : 'gray';
+        }
+
+        if (button === lastClick) {
+            return 'orange';
+        }
+
+        if (button === firstClick) {
+            return 'green';
+        }
+
+        return 'gray';
+    };
+
+    const firstClick = clicks[0];
+    const lastClick = clicks[clicks.length - 1];
+
     return (
         <div className="TestPage"> 
             <Header title={`Kandidat : ${user.name} ${user.surname}`} />
         <Container>
-        <Row>
-            <Col sm={8}>
+    <Row>
+        <Col sm={8}>
             <div className="buttons">
-            <BaseButton color="gray">1</BaseButton>
-            <BaseButton color="gray">2</BaseButton>
-            <BaseButton color="gray">3</BaseButton>
-            <BaseButton color="gray">4</BaseButton>
-            <BaseButton color="gray">5</BaseButton>
+                {buttons.map((button) => (
+                    <BaseButton key={button} onClick={handleClick(button)} color={getButtonColor(button)}>
+                       {button}
+                    </BaseButton>
+                ))}
             </div>
+        </Col>
+
+        <Col sm={4}>
+            {isRecording ? (
+                <BaseButton onClick={handleStopClick} color="orange">
+                    Stop
+                </BaseButton>
+            ) : (
+                <div>
+                    <div>Prvi klik : dugme br. {firstClick}</div>
+                    <div>Poslednji klik : dugme br. {lastClick}</div>
+                </div>
+            )}
+        </Col>
+    </Row>
+    {isRecording ? null : (
+        <Row>
+            <Col sm={3}>
+                <BaseButton color="red">Reset</BaseButton>
             </Col>
-            <Col sm={4}>
-            <BaseButton color="orange">Stop</BaseButton>
+            <Col sm={3}>
+                <BaseButton color="blue">User Info</BaseButton>
+            </Col>
+            <Col sm={3}>
+                <BaseButton color="blue">Statistika</BaseButton>
+            </Col>
+            <Col sm={3}>
+                <BaseButton color="blue">Izađi</BaseButton>
             </Col>
         </Row>
-        <Row>
-         <Col sm={3}>
-         <BaseButton color="red">Reset</BaseButton>
-         </Col>
-         <Col sm={3}>
-         <BaseButton color="blue">User info</BaseButton>
-         </Col>
-         <Col sm={3}>
-         <BaseButton color="blue">Stetistika</BaseButton>
-         </Col>
-         <Col sm={3}>
-         <BaseButton color="blue">Izadji</BaseButton>
-         </Col>
-         </Row>
-        
+    )}  
         </Container>
 
-        <Modal open={true} title="User Info">
+        <Modal open={false} title="User Info">
                 Modal test content
             </Modal>
         </div>
